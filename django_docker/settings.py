7c35f9da482d8d447650fc98102dc47197949ef9
Fabricio Duarte <fabricioaduarte@gmail.com>
2016-02-24 01:07:53 -0200

    changed the db to postgres

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'django_docker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dockerdb',
+        'USER': 'asapadmin',
+        'PASSWORD': 'asapadmin',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
